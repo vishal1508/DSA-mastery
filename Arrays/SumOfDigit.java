c19bcf0b3a38b526755bd50b2e961131073a2532
vishal1508 <vishalkerlekar15@gmail.com>
2025-08-20 16:02:42 +0530

    add Sum of Digit

--- Arrays/SumOfDigit.java
+++ Arrays/SumOfDigit.java
@@ -3,7 +3,7 @@ public class SumOfDigit{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     
-    int a[] = {1,2,3,5,7,10,11,15};
+    int a[] = {1,2,3,5,10,11,15};
 
     int target = 15;
 
