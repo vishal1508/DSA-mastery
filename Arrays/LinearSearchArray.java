7a60ebd3a4490ae7c66be93e6440a24b75b49bf4
vishal1508 <vishalkerlekar15@gmail.com>
2025-09-10 10:41:52 +0530

    Fix smmall bug in Linear Search

--- Arrays/LinearSearchArray.java
+++ Arrays/LinearSearchArray.java
@@ -1,16 +1,21 @@
+import java.util.Scanner;
 public class LinearSearch{
 	public static void main(String... args){
 		int[] a = {23,24,32,12,10,34};
-		
-		int searchNumber = 12;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Number To Search := ");		
+		int searchNumber = sc.nextInt();
 		int i = 0;
-		while(i < a.length - 1){
-			if(a[i] != searchNumber){
-				i = i + 1;
-			}else if(a[i] == searchNumber){
+		boolean found = false;
+		while(i < a.length){	
+		 if(a[i] == searchNumber){
 				System.out.printf("%d is Found at index %d th",searchNumber,i);
+				found = true;
 				break;
 			}
+			i++;
 		}
-	}
-}
+		if(!found){
+			            System.out.printf("%d is not found in the array%n", searchNumber);
+		}
+}}
