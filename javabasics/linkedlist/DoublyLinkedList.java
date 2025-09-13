ed298e1d33c49738ae3ba843666decf14e5f69b0
vishal1508 <vishalkerlekar15@gmail.com>
2025-09-13 17:28:38 +0530

    implement and Understanding of InsertAtBegining and InsertAtEnd, Display DublyLinked List

---
+++ javabasics/linkedlist/DoublyLinkedList.java
@@ -0,0 +1,66 @@
+package linkedlist;
+
+class Node {
+  int data;
+  Node prev;
+  Node next;
+
+  Node(int data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyList {
+  Node head;
+
+  public void insertAtBeginning(int data) {
+    Node newData = new Node(data);
+    if (head == null) {
+      head = newData;
+      return;
+    }
+
+    newData.next = head;
+    head.prev = newData;
+    head = newData;
+
+  }
+
+  public void insertAtEnd(int data) {
+    Node newData = new Node(data);
+    if (head == null) {
+      head = newData;
+      return;
+    }
+    Node temp = head;
+    while (temp.next != null) {
+      temp = temp.next;
+    }
+    temp.next = newData;
+    newData.prev = temp;
+
+  }
+
+  public void displayDubluyLinkedlist() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.printf("%d ->", temp.data);
+      temp = temp.next;
+    }
+  }
+}
+
+public class DoublyLinkedList {
+  public static void main(String[] args) {
+    DoublyList d = new DoublyList();
+    // d.insertAtBeginning(10);
+    // d.insertAtBeginning(20);
+    // d.insertAtBeginning(30);
+    d.insertAtEnd(20);
+    d.insertAtEnd(30);
+    d.insertAtBeginning(10);
+    d.displayDubluyLinkedlist();
+  }
+}
