061e2f4d8fa7eff91e95ed0266ccc66c6f963d38
vishal1508 <vishalkerlekar15@gmail.com>
2025-09-14 21:45:03 +0530

    implement and understanding Dubly Link List method like InsertAfterNode

--- Arrays/linkedlist/DoublyLinkedList.java
+++ Arrays/linkedlist/DoublyLinkedList.java
@@ -43,8 +43,33 @@ class DoublyList {
 
   }
 
+  public void insertAfterNoder(int data, int afterNode) {
+    Node temp = head;
+    boolean isFound = false;
+    while (temp != null) {
+      if (afterNode == temp.data) {
+        isFound = true;
+        break;
+      }
+      temp = temp.next;
+    }
+    if (!isFound) {
+      System.out.printf("\n %d this node is not exist so we cant insert!!!", afterNode);
+      return;
+    }
+    Node newData = new Node(data);
+    newData.next = temp.next;
+    temp.next = newData;
+    newData.prev = temp;
+    if (newData.next != null) {
+      newData.next.prev = newData;
+    }
+    return;
+  }
+
   public void displayDubluyLinkedlist() {
     Node temp = head;
+
     while (temp != null) {
       System.out.printf("%d ->", temp.data);
       temp = temp.next;
@@ -61,6 +86,7 @@ public class DoublyLinkedList {
     d.insertAtEnd(20);
     d.insertAtEnd(30);
     d.insertAtBeginning(10);
+    d.insertAfterNoder(25, 20);
     d.displayDubluyLinkedlist();
   }
 }
