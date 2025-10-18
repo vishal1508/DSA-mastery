489953fbc34e97022af6263110ece69158e20c75
vishal1508 <vishalkerlekar15@gmail.com>
2025-10-18 17:00:00 +0530

    delete thread practice folder

--- javabasics/inheritance/herachicleinheritance/HerachicleInheritance.java
+++ javabasics/inheritance/herachicleinheritance/HerachicleInheritance.java
@@ -2,11 +2,11 @@ package inheritance.herachicleinheritance;
 
 public class HerachicleInheritance {
   public static void main(String[] args) {
-    Circle c = new Circle();
-    Rectangle r = new Rectangle();
-    c.circleArea(4);
+//    Circle c = new Circle();
+//    Rectangle r = new Rectangle();
+//    c.circleArea(4);
 
-    r.rectangleArea(12, 13);
+//    r.rectangleArea(12, 13);
 
   }
 }
