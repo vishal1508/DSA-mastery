bdfc2838ad34290d765ee333cb21e24ba248a9c6
vishal1508 <vishalkerlekar15@gmail.com>
2025-10-18 18:35:32 +0530

    feat: add multithreading essentials example (priority, interrupt, yield)

--- javabasics/multithread/MultiThreadEssentialMethodsExample.java
+++ javabasics/multithread/MultiThreadEssentialMethodsExample.java
@@ -1,21 +1,61 @@
 package multithread;
-
-public class MultiThreadEssentialMethodsExample extends Thread {
-    MultiThreadEssentialMethodsExample(String threadName){
+class SetPriority extends Thread{
+    SetPriority(String threadName){
         super(threadName);
     }
     @Override
     public void run() {
-        for(int i = 0; i < 5; i++){
-            System.out.println(Thread.currentThread().getName() +" Priority : "+Thread.currentThread().getPriority()+" Count : "+i);
-        }
-        System.out.println("Running");
-    }
-    public static void main(String[] args) throws InterruptedException {
-        MultiThreadEssentialMethodsExample low = new MultiThreadEssentialMethodsExample("low");
-        MultiThreadEssentialMethodsExample medium = new MultiThreadEssentialMethodsExample("medium");
-        MultiThreadEssentialMethodsExample high = new MultiThreadEssentialMethodsExample("High");
+        try{
+            for(int i = 0; i < 5; i++){
+                System.out.println(Thread.currentThread().getName() +" Priority : "+Thread.currentThread().getPriority()+" Count : "+i);
+            }
+            System.out.println("Running");
 
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
+class InterruptExample extends  Thread{
+    @Override
+    public void run() {
+        try{
+         Thread.sleep(1000);
+        System.out.println("Intrrupt Running");
+        }catch(InterruptedException e){
+            System.out.println("Thread is intrrupted while sleeping");
+        }
+    }
+}
+class YeildExample extends Thread{
+    YeildExample(String threadName){
+        super(threadName);
+    }
+    @Override
+    public void run() {
+        try{
+            Thread.sleep(1000);
+            for(int i = 0; i < 5; i++){
+                System.out.println(Thread.currentThread().getName()+" is Running");
+            }
+            Thread.yield();
+        }catch (Exception e){
+            System.out.println("Exception");
+        }
+    }
+}
+public class MultiThreadEssentialMethodsExample extends Thread {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        SetPriority low = new SetPriority("low");
+        SetPriority medium = new SetPriority("medium");
+        SetPriority high = new SetPriority("High");
+
+        InterruptExample intterup = new InterruptExample();
+        YeildExample task1 = new YeildExample("Task1");
+        YeildExample task2 = new YeildExample("Task2");
+        YeildExample task3 = new YeildExample("Task3");
         low.setPriority(MIN_PRIORITY);
         medium.setPriority(NORM_PRIORITY);
         high.setPriority(MAX_PRIORITY);
@@ -23,7 +63,15 @@ public class MultiThreadEssentialMethodsExample extends Thread {
         low.start();
         medium.start();
         high.start();
-        Thread.sleep(2000);
+        high.interrupt();
+
+
+        intterup.start();
+        intterup.interrupt();
+
+        task1.start();
+        task2.start();
+        task3.start();
 
     }
 
